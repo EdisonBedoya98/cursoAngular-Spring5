73b620a228130c899eb548bc3db9575bb3306be5
EdisonBedoya98 <edison.bedoyag@udea.edu.co>
2020-01-26 15:55:33 -0500

    update

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {CLIENTES} from './clientes.json';
+import { Cliente } from './cliente.js';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  private urlEndPoint:string = 'http://localhost:8081/api/clientes'
+  constructor(private http: HttpClient) { }
+
+  getClientes():Observable<Cliente[]> {
+    // return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
+   }
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+    {id: 2, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+    {id: 3, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+    {id: 4, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+    {id: 5, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+    {id: 6, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+    {id: 7, nombre:'Edison',apellido:'Bedoya',email:'edison@uno.dos.com', createAt:'2020-01-13'},
+
+  ];
